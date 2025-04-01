a4156981250534ebe8d61b17890a51bed6dac4b5
Will Herrick <willz@Wills-MacBook-Air.local>
2025-04-01 15:34:53 -0400

    Bot sends messages to server

--- discord-bot/src/main.py
+++ discord-bot/src/main.py
@@ -1,10 +1,15 @@
-import discord # type: ignore
+import discord  # type: ignore
 from config.token import TOKEN
 
 # Creates a client connection to Discord
 class Client(discord.Client):
-    async def on_ready(self):   # Event handler for when the bot is ran
+    async def on_ready(self):  # Event handler for when the bot is ready
         print(f'Logged in as {self.user}')
+        
+        # Message to server test
+        channel = discord.utils.get(self.get_all_channels(), name="general")
+        if channel:
+            await channel.send("Hi!")
 
 # Gains access to bot intents given through developer portal
 intents = discord.Intents.default()
