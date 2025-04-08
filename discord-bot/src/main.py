3a59a5cc40cce6055f4b8d3d9ab8de75bb10ed83
Will Herrick <willz@Wills-MacBook-Air.local>
2025-04-07 22:01:05 -0400

    Scraped old code, rebuilding with bsky API

--- discord-bot/src/main.py
+++ discord-bot/src/main.py
@@ -1,20 +1,28 @@
 import discord  # type: ignore
+from discord.ext import tasks  # type: ignore
+import requests  # type: ignore
+from bs4 import BeautifulSoup  # type: ignore
+
 from config.token import TOKEN
 
-# Creates a client connection to Discord
-class Client(discord.Client):
-    async def on_ready(self):  # Event handler for when the bot is ready
-        print(f'Logged in as {self.user}')
-        
-        # Message to server test
-        channel = discord.utils.get(self.get_all_channels(), name="general")
-        if channel:
-            await channel.send("Hi!")
+# Toine Lay card leaks account (Bluesky)
+ToineLayBsky = "https://bsky.app/profile/toinelay.bsky.social"
 
 # Gains access to bot intents given through developer portal
 intents = discord.Intents.default()
 intents.message_content = True
 
+# Discord bot setup
+class Client(discord.Client):
+    async def on_ready(self):
+        print(f'Logged in as {self.user}')
+        
+        # Test by sending the latest post immediately when the bot is ready
+        channel = discord.utils.get(self.get_all_channels(), name="general")
+        if channel:
+            await channel.send("Bot is online")
+            
 # Creates bot and runs it
 client = Client(intents=intents)
 client.run(TOKEN)
+
